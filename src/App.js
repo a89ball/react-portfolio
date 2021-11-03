58c030f7cbfb9443e64fbbda72d8b0bf3514600b
Abdifatah <abdifatah.101@outlook.com>
2021-11-03 21:53:56 +0000

    progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import NavBar from './src/components/NavBar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Clients from './src/components/Clients'
-import Skills from './src/components/Skills'
-import Projects from './src/components/Project';
-import Contact from './src/components/Contact';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Clients from './components/Clients'
+import Skills from './components/Skills'
+import Projects from './components/Project';
+import Contact from './components/Contact';
 //import Footer from './components/Footer';
-import About from './src/components/About';
+import About from './components/About';
 
 function App() {
     return (
       
   <Router>
   <NavBar />
-  <Switch>
+  <Routes>
     <Route path='/' exact component={About}/>
     <Route path='/clients' exact component={Clients}/>
     <Route path='/projects' exact component={Projects}/>
     <Route path='/skills' exact component={Skills}/>
     <Route path='/contact' exact component={Contact}/>
-  </Switch>
+  </Routes>
   </Router>
     );
   }
